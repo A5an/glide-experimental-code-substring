932e36fcca93db942aa5ca9986ba852cab87aa85
Aukenov Assanali <115568332+A5an@users.noreply.github.com>
2024-05-28 17:53:22 +0000

    Initial commit

---
+++ function.js
@@ -0,0 +1,19 @@
+// The function here takes the parameters that you
+// have declared in the `glide.json` file, in the
+// same order.
+window.function = function (str) {
+  // For each parameter, its `.value` contains
+  // either its value in the type you've declared,
+  // or it's `undefined`.  This is a good place to
+  // extract the `.value`s and assign default
+  // values.
+  str = str.value ?? "";
+  const numbers = str.match(/\d+/g).map(Number);
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  // Your function should return the exact type
+  // you've declared for the `result` in
+  // `glide.json`, or `undefined` if there's an
+  // error or no result can be produced, because a
+  // required input is `undefined`, for example.
+  return sum;
+};
